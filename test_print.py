579488a2dd375bdeaac23bd6bc71acb7b917aaa6
MaybeShah <liyakatshaikh07@gmail.com>
2020-08-05 14:28:04 +0530

    delete unnecessary files

--- test_print.py
+++ test_print.py
@@ -7,6 +7,9 @@ printer = getUSBPrinter()(idVendor=0x1504,
                           inputEndPoint=0x82,
                           outputEndPoint=0x01) #Create the printer object with the connection params
 '''
+
+
+
 from escpos.connections import getNetworkPrinter
 
 
@@ -14,6 +17,9 @@ printer = getNetworkPrinter()(host='192.168.1.87', port=9100)
 #printer.text("Hello World")
 printer.image('C:/Users/Maybe_shah/Documents/flask_app/FaceMask/table.png') #"C:\Users\Maybe_shah\Documents\flask_app\FaceMask\table.png"
 printer.lf()
+
+
+
 '''from escpos.connections import getNetworkPrinter
 
 
